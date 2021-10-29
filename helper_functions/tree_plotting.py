d77939a8063555fc3a2ffb018d0aee3e2c41f4e9
petraratkai <pr2518@imperial.ac.uk>
2021-10-29 09:22:29 +0100

    finalising tree_plotting

--- helper_functions/tree_plotting.py
+++ helper_functions/tree_plotting.py
@@ -1,14 +1,15 @@
+import matplotlib 
 import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_tree_helper(root, depth=0, x=0, y=50, yspacing=5):
     if isinstance(root, str) or isinstance(root, int) or isinstance(root, float): #come back later probs string
         #draw leaf node:
-        plt.text(x, y, "leaf: " + str(root), size='smaller', rotation=0,
+        plt.text(x, y, str(int(root)), size='smaller', rotation=0,
          ha="center", va="center",
          bbox=dict(boxstyle="round",
-                   ec=(1., 0.5, 0.5),
-                   fc=(1., 0.8, 0.8),
+                   ec=(0., 0., 0.),
+                   fc=(1., 1., 1.),
                    )
          )
         return
@@ -16,15 +17,15 @@ def plot_tree_helper(root, depth=0, x=0, y=50, yspacing=5):
     plt.text(x, y, str("x" + str(root['attribute'])) + " < " + str(root['value']), size='smaller', rotation=0,
          ha="center", va="center",
          bbox=dict(boxstyle="round",
-                   ec=(1., 0.5, 0.5),
-                   fc=(1., 0.8, 0.8),
+                   ec=(0., 0., 0.),
+                   fc=(1., 1., 1.),
                    )
          )
     #have to draw left line and right line, so calc xchildren, ychildren
     yChildLeft = y - yspacing
     yChildRight = y - yspacing
-    xChildLeft = x - 1/np.power(2, depth)
-    xChildRight = x + 1/np.power(2, depth)
+    xChildLeft = x - 2*1/np.power(2, depth)
+    xChildRight = x + 2*1/np.power(2, depth)
     x_values = [xChildLeft, x, xChildRight]
     y_values = [yChildLeft, y, yChildRight]
     plt.plot(x_values, y_values)
@@ -36,7 +37,8 @@ def plot_tree_helper(root, depth=0, x=0, y=50, yspacing=5):
 
 
 def plot_tree(root, maxdepth, filename) :
-    figure(figsize=(min(2**maxdepth, 2**9), maxdepth), dpi=80)
+    matplotlib.use('Agg') 
+    figure(figsize=(min(2**maxdepth, 2**7), maxdepth), dpi=80)
     plot_tree_helper(root)
     #plt.xlim([-3,3])
     plt.axis('off')
